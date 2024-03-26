d1320e08c31a1316a364fb9bdee3aa3606e270f6
ryoma kawahara <ryoma.k.0224@gmail.com>
2024-03-26 22:58:04 +0900

    completed

--- content.js
+++ content.js
@@ -8,67 +8,62 @@ const labelButtonClassMap = {
 };
 
 function setupMutationObserver() {
-  // 監視対象のノードを指定 (document.bodyを監視対象とする)
   const targetNode = document.body;
-
-  // MutationObserverの設定
   const config = { childList: true, subtree: true };
-
-  // MutationObserverのコールバック関数
-  const callback = function(mutationsList, observer) {
-    // ラベルに基づいてマージオプションをハイライト
+  const callback = function (mutationsList, observer) {
     highlightBasedOnLabels();
-    // 必要に応じて監視を解除
-    // observer.disconnect();
   };
-
-  // MutationObserverのインスタンスを作成
   const observer = new MutationObserver(callback);
 
-  // 監視を開始
   observer.observe(targetNode, config);
 }
 
-function highlightOtherMergeOptionsExcluding(label) {
+function highlightMergeButton(label) {
   const btn = document.querySelector(`.${labelButtonClassMap[label]}`);
-  console.log('Highlighting button:', btn);
+  if (!btn) {
+    return;
+  }
 
   btn.style.backgroundColor = 'red';
   btn.style.color = 'white';
 }
 
-function highlightBasedOnLabels() {
+// Find the allowed merge label from the PR page
+function findAllowedLabel() {
   const labelNames = Array.from(new Set(Array.from(document.querySelectorAll(labelSelector)).map(label => label.innerText.trim().toLowerCase())));
 
+  // No Support for multiple labels
   let allowedMergeLabel;
   Object.keys(labelButtonClassMap).forEach(label => {
     if (labelNames.includes(label)) {
       allowedMergeLabel = label;
 
-      // No Support for multiple labels
       return;
     }
   });
 
-  if (allowedMergeLabel) {
-    console.log('Allowed merge label:', allowedMergeLabel);
+  return allowedMergeLabel;
+}
 
+function highlightBasedOnLabels() {
+  const allowedMergeLabel = findAllowedLabel();
+
+  if (allowedMergeLabel) {
     Object.entries(labelButtonClassMap).forEach(([key, value]) => {
       if (key !== allowedMergeLabel) {
-        highlightOtherMergeOptionsExcluding(key);
+        highlightMergeButton(key);
       }
     });
   }
 }
 
-function checkURLAndHighlightMergeOptions() {
-  // TODO: domain should be configurable
+function main() {
   // Check if the current URL is a Github OR Github Enterprize PR page.
-  const urlPattern = /^https:\/\/github\.hoge\.jp\/.+\/.+\/pull\/.+$/;
+  const urlPattern = /^https:\/\/github\..+\/.+\/.+\/pull\/.+$/;
   if (urlPattern.test(window.location.href)) {
     console.log('Github PR page detected');
     setupMutationObserver();
   }
 }
 
-window.onload = checkURLAndHighlightMergeOptions;
+window.onload = main;
